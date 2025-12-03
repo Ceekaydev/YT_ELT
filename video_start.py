dd83b2c4efbb741a975f494391adf21c390e4ffe
Okeke Chukwudi <okekeceekay@gmail.com>
2025-12-03 13:33:54 +0100

    update the code to get video ids of all videos

--- video_start.py
+++ video_start.py
@@ -9,6 +9,7 @@ load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
 channel_name = "MrBeast"
+maxResults = 50
 
 def get_playlist_id():
 
@@ -40,13 +41,53 @@ def get_playlist_id():
     except Exception as e:
         print(f"Error: {e}")
         return False
+    
+
+def get_video_ids(playlist_id):
+
+    video_ids = []
+
+    pageToken = None
+
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&playlistId={playlist_id}&maxResults={maxResults}&key={API_KEY}"
+
+    try:
+
+        while True:
+
+            url = base_url
+
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+
+            response = requests.get(url)
+
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get("items", []):
+                video_id = item["contentDetails"]["videoId"]
+                video_ids.append(video_id)
+
+            pageToken = data.get("nextPageToken")
+
+            if not pageToken:
+                break
+
+        return video_ids
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+
+
 
 if __name__ == "__main__":
-    success = get_playlist_id()
+    playlist_id = get_playlist_id()
+    video_ids = get_video_ids(playlist_id)
 
-    if success:
-        print(f"successful. ID: {success}")
-    else:
-        print("Unsuccessful, Check code.")
+
 
 
