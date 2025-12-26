866ad1b4ccc71c97c08474c74aa6eb7ad740ae36
Okeke Chukwudi <okekeceekay@gmail.com>
2025-12-26 19:38:09 +0100

    same fix

--- dags/main.py
+++ dags/main.py
@@ -79,7 +79,7 @@ with DAG(
     trigger_update_db = TriggerDagRunOperator(
         task_id="trigger_Update_db",
         trigger_dag_id="Update_db",
-        conf="{{ ti.xcom_pull(task_ids='prepare_trigger_conf') }}",
+        conf="{{ ti.xcom_pull(task_ids='prepare_trigger_conf') | tojson }}",
     )
 
     # define dependencies
